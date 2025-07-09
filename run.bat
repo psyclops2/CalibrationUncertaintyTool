09520c0f1644d2808529397693dd6a397fce5b22
psyclops2 <psyclops_2@hotmail.com>
2025-07-09 19:28:13 +0900

    .qmの生成追加

--- run.bat
+++ run.bat
@@ -2,6 +2,14 @@
 REM Activate virtual environment
 call venv\Scripts\activate
 
+REM Build translation files
+echo Building translation files...
+pushd src
+if not exist i18n\translations mkdir i18n\translations
+pyside6-lupdate -extensions py,ui -ts i18n/translations/translations_ja.ts i18n/translations/translations_en.ts
+pyside6-lrelease i18n/translations/*.ts
+popd
+
 REM Run the application
 echo Starting the application...
 python -m src
