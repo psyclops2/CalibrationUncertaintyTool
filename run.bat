cb75e00b5ffb84b7fd61aace29e06d33d5fddd0b
psyclops2 <psyclops_2@hotmail.com>
2025-07-09 19:34:42 +0900

    .qm生成させたい

--- run.bat
+++ run.bat
@@ -2,11 +2,18 @@
 REM Activate virtual environment
 call venv\Scripts\activate
 
-REM Build translation files
-echo Building translation files...
+REM Generate and build translation files
+echo Generating translation files...
 pushd src
+python -m i18n.generate_translations
+
+REM Update TS files with new strings from source code
+echo Updating translation files from source...
 if not exist i18n\translations mkdir i18n\translations
 pyside6-lupdate -extensions py,ui -ts i18n/translations/translations_ja.ts i18n/translations/translations_en.ts
+
+REM Compile TS to QM
+echo Compiling translation files...
 pyside6-lrelease i18n/translations/*.ts
 popd
 
