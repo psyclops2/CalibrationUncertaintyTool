112967f520ec4b2b8de6e9d42d1e43f96978adcb
psyclops2 <psyclops_2@hotmail.com>
2025-07-12 00:30:08 +0900

    修正

--- run.bat
+++ run.bat
@@ -7,18 +7,12 @@ echo Generating translation files...
 pushd src
 python -m i18n.generate_translations
 
-REM Update TS files with new strings from source code
-echo Updating translation files from source...
-if not exist i18n\translations mkdir i18n\translations
-pyside6-lupdate -extensions py,ui -ts i18n/translations/translations_ja.ts i18n/translations/translations_en.ts
-
 REM Compile TS to QM
 echo Compiling translation files...
-pyside6-lrelease i18n/translations/*.ts
+pyside6-lrelease i18n/ja.ts
+pyside6-lrelease i18n/en.ts
 popd
 
 REM Run the application
 echo Starting the application...
 python -m src
-
-pause
